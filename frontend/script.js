76f949db55afc79851669769e09c046bf95529a4
Pizaan <tadiwala@sheridancollege.ca>
2023-12-22 11:54:20 -0500

    Added Folder structure

---
+++ frontend/script.js
@@ -0,0 +1,28 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  getVisitCount();
+});
+
+const functionApi = '';
+
+const getVisitCount = () => {
+  let count = 30;
+  fetch(functionApi)
+    .then(response => {
+      return response.json()
+    })
+    .then(response => {
+      console.log("Website called function API.");
+      count = response.count;
+      document.getElementById("counter").innerText = count;
+    }).catch(function (error) {
+      console.log(error);
+    });
+  return count;
+}
